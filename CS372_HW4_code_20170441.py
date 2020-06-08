6e95f0319df805df0c5cb7b74aa12c9f8591b98d
yunhyeeun <dmsdbsgP0320@gmail.com>
2020-06-08 13:56:47 +0900

    Complete

--- CS372_HW4_code_20170441.py
+++ CS372_HW4_code_20170441.py
@@ -1,72 +1,10 @@
 import nltk, re, random, csv
-from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk import Tree
-from nltk import ne_chunk
 from nltk.tree import ParentedTree
 from nltk.tree import MultiParentedTree
-from nltk.tag import pos_tag
 from nltk.chunk import RegexpParser
-from nltk.corpus import wordnet as wn
 from allennlp.predictors.predictor import Predictor
 import allennlp_models.syntax.constituency_parser
-import allennlp_models.syntax.biaffine_dependency_parser
-f = open(r"input_hw4.txt", "r")
-openedFile = f.readlines()
-f.close()
-text = ""
-for line in openedFile:
-    text += line
-dataObject = text.split("\n---")
-
-activateList = []
-inhibitList = []
-bindList = []
-requireList = []
-preventList = []
-actionList = ["activate", "activates", "activated",
-              "inhibit", "inhibits", "inhibited",
-              "bind", "binds",
-              "require", "requires", "required",
-              "prevent", "prevents", "prevented"]
-preposition = ["in", "on", "of", "at", "with", "by"]
-cmudict = nltk.corpus.cmudict.dict()
-def putActionList(obj):
-    verb = obj["verb"]
-    if verb == "activate":
-        activateList.append(obj)
-    elif verb == "inhibit":
-        inhibitList.append(obj)
-    elif verb == "bind":
-        bindList.append(obj)
-    elif verb == "require":
-        requireList.append(obj)
-    elif verb == "prevent":
-        preventList.append(obj)
-    else:
-        print("CANNOT FIND VERB")
-
-def makeObject(text):
-    text = text.strip("\n")
-    cgs = text.split("\n")
-    verb = ""
-    sentence = ""
-    triplet = ""
-    cited = ""
-    for cg in cgs:
-        cg = cg.strip()
-        if "Verb: " in cg:
-            verb = cg.split("Verb: ")[1]
-        elif "Sentence: " in cg:
-            sentence = cg.split("Sentence: ")[1]
-            re.sub(r'\(.*?\)', '', sentence)
-        elif "Triplet: " in cg:
-            triplet = cg.split("Triplet: ")[1]
-            triplet = triplet.split(", ")
-        elif "Cited: " in cg:
-            cited = cg.split("Cited: ")[1]
-        else:
-            break
-    return {"verb": verb, "sentence": sentence, "triplet": triplet, "cited": cited}
 
 def makeTestList(lists):
     testidx = random.sample(range(0,20), 4)
@@ -79,120 +17,16 @@ def makeTestList(lists):
         trainingList += [l[x] for x in trainingidx]
     return testList, trainingList
 
-def wnPos2nltkPos(pos):
-    if pos == "n":
-        return "NN"
-    elif pos == "a" or pos == "s":
-        return "JJ"
-    elif pos == "v":
-        return "VB"
-    elif pos == "r":
-        return "RB"
-    else:
-        return None
-
-def morePos(pos):
-    newPos = []
-    for w, p in pos:
-        if w == "," or w == "and" or w == ".":
-            newPos.append((w, p))
-            continue
-        if w == "to":
-            newPos.append((w, "TO"))
-            continue
-        if w in preposition:
-            newPos.append((w, "IN"))
-            continue
-        if cmudict.get(w, "empty") == "empty":
-            newPos.append((w, "NN"))
-            continue
-        synsets = wn.synsets(w)
-        if len(synsets) == 0:
-            newPos.append((w, p))
-            continue
-        newP = wnPos2nltkPos(synsets[0].pos())
-        if newP != None and p != newP:
-            if (newP != "NN"):
-                p = newP
-        if w in actionList and p.startswith("V") == False:
-            newPos.append((w, "VB"))
-            continue
-        else:
-            newPos.append((w, p))
-    return newPos
-
-grammar = r"""
-    NP: { <DT|PRP>? <JJ.*>* <NN.*>+ }
-    XNP: { <CC|,> <NP> }
-    CNP: { <NP> <XNP>+ }
-    V: { <VBD|VBZ> <VB.*> <IN> | <VB.*>+ | }
-    VP: { <V>+ <CNP| NP | PP>* }
-    XVP: { <CC|,> <VP> }
-    CVP: { <VP> <XVP>+ }
-    PP: { <IN|TO> <NP|CNP> }
-"""
-chunk_parser = RegexpParser(grammar)
-constPredictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/elmo-constituency-parser-2020.02.10.tar.gz")
-dependPredictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/biaffine-dependency-parser-ptb-2020.04.06.tar.gz")
-
-for i in range(100):
-    obj = makeObject(dataObject[i])
-    putActionList(obj)
-dataLists = [activateList, inhibitList, bindList, requireList, preventList]
-testList, trainingList = makeTestList(dataLists)
-trainingSent = [obj["sentence"] for obj in trainingList]
-trainingTriplet = [obj["triplet"] for obj in trainingList]
-
-# EXAMPLE_OBJECT = activateList[0]
-# EXAMPLE_SENTENCE = EXAMPLE_OBJECT["sentence"]
-# EXAMPLE_TRIPLET = EXAMPLE_OBJECT["triplet"]
-# result = predictor.predict(
-#   sentence= EXAMPLE_SENTENCE
-# )
 def find_phrase (tree, phrase):
     leaves = [subtree for subtree in tree if type(subtree) == Tree and subtree.label() == phrase]
     return leaves
-# EXAMPLE_TREES = nltk.Tree.fromstring(result["trees"])
-# print (find_phrase(EXAMPLE_TREES, "VP"))
 
-f = open ('CS372_HW4_output_20170441.csv', 'w', newline = '', encoding='utf-8-sig')
-wr = csv.writer(f)
-def findVP(inputList):
-    for i in range(20):
-        EXAMPLE_OBJECT = inputList[i]
-        EXAMPLE_SENTENCE = EXAMPLE_OBJECT["sentence"]
-        EXAMPLE_TRIPLET = EXAMPLE_OBJECT["triplet"]
-        wr.writerow(["Sentence:{}".format(i)])
-        wr.writerow([EXAMPLE_SENTENCE])
-        wr.writerow(EXAMPLE_TRIPLET)
-        
-        # constResult = constPredictor.predict(
-        #     sentence = EXAMPLE_SENTENCE
-        # )
-        dependResult = dependPredictor.predict(
-            sentence = EXAMPLE_SENTENCE
-        )
-        print (dependResult.items())
-        # EXAMPLE_CONST_TREES = constResult["trees"]
-        # EXAMPLE_DEPEND_TREES = dependResult["trees"]
-        # wr.writerow(nltk.Tree.fromstring(EXAMPLE_CONST_TREES))
-        # wr.writerow(nltk.Tree.fromstring(EXAMPLE_DEPEND_TREES))
-        
-        # wr.writerow(find_phrase(nltk.Tree.fromstring(EXAMPLE_TREES), "VP"))
-        wr.writerow(["------"])
-        break
-def makeConstTree(sent):
-    constResult = constPredictor.predict(
+def makeConstTree(sent, predictor):
+    constResult = predictor.predict(
         sentence = sent
     )
-    return ParentedTree.fromstring(constResult["trees"])
+    return constResult
 
-def findSubject(t):
-    s = []
-    for subtree in t.subtrees():
-        if subtree.label() == "NP":
-            s.append(subtree)
-    return s
 def checkPassive(t):
     parent = t.parent()
     may_pp = t.right_sibling()
@@ -202,7 +36,7 @@ def checkPassive(t):
             return may_be.leaves()[0] + " " + " ".join(t.leaves()) + " by"
     return " ".join(t.leaves())
 
-def findVerbs(t, phrase, verb):
+def findVerbs(t, phrase, verb, actionList):
     for subtree in t:
         if type(subtree) == ParentedTree:
             if subtree.label().startswith("VB") and subtree.leaves()[0] in actionList:
@@ -210,77 +44,189 @@ def findVerbs(t, phrase, verb):
                 v = checkPassive(subtree)
                 phrase.append(parent)
                 verb.append(v)
-            findVerbs(subtree, phrase, verb)
-    
-def findSubSent(t):
-    s = []
-    for subtree in (t.subtrees()):
-        if subtree.label() == "S":
-            s.append(subtree)
-    return s
-def findObject(vps):
-    objects = []
+            findVerbs(subtree, phrase, verb, actionList)
+
+def findSubject(vps, actionList):
+    subjectP = []
     for idx, vp in enumerate (vps):
-        newVp = MultiParentedTree.convert(vp)
-        for subtree in newVp:
+        for subtree in vp:
             if subtree.label().startswith("VB") and subtree.leaves()[0] in actionList:
-                rightSiblings = subtree.right_siblings()
-                objectPhrase = []
-                for i, sibling in enumerate (rightSiblings):
-                    if rightSiblings[0].label() == "PP":
-                        objectPhrase.append(rightSiblings[0])
+                subjectPhrase = subtree
+                while subjectPhrase != None:
+                    if subjectPhrase.left_sibling() != None and subjectPhrase.left_sibling().label() in ["NP", "PRP"]:
+                        subjectP.append(subjectPhrase.left_sibling())
                         break
-                    elif rightSiblings[i].label() == "NP":
-                        objectPhrase.append(sibling)
-                    elif rightSiblings[i].label() == "CC":
-                        continue
                     else:
-                        break
-                for p in objectPhrase:
-                    objects.append(" ".join(p.leaves()))
-    return objects
-for l in dataLists:
-    # findVP(l)
-    for obj in l:
-        EXAMPLE_SENTENCE = obj["sentence"]
-        EXAMPLE_TRIPLET = obj["triplet"]
-        wr.writerow([EXAMPLE_SENTENCE])
-        wr.writerow(EXAMPLE_TRIPLET)
-        constTree = makeConstTree(EXAMPLE_SENTENCE) 
-        verbPhrase = []
-        verbs = []
-        findVerbs(constTree, verbPhrase, verbs)
-        objects = findObject(verbPhrase)
-        for i in range(len(verbs)):
-            wr.writerow([verbs[i]])
-            wr.writerow([objects[i]] if i < len(objects) else ["NOT EXIST"])
-        # for phrase in verbPhrase:
-            # wr.writerow(verbPhrase)
-        wr.writerow(["-----"])
-    # constTree.draw()
-    # verb = findVerb(constTree)
-    # for v in actionList:
+                        subjectPhrase = subjectPhrase.parent()
+    return subjectP
 
-f.close()
+def findObject(vps, actionList):
+    objectP = []
+    for idx, vp in enumerate (vps):
+        for subtree in vp:
+            if subtree.label().startswith("VB") and subtree.leaves()[0] in actionList:
+                objectPhrase = subtree
+                if objectPhrase != None:
+                    if objectPhrase.right_sibling() != None and objectPhrase.right_sibling().label() == "PP":
+                        tmp = [subtree for subtree in objectPhrase.right_sibling() if subtree.label().startswith("N")]
+                        objectP.append(tmp[0] if len(tmp) > 0 else "")
+                    elif objectPhrase.right_sibling() != None and (objectPhrase.right_sibling().label().startswith("N") or objectPhrase.right_sibling().label() == "PRP"):
+                        objectP.append(objectPhrase.right_sibling())
+                    else:
+                        objectP.append("")
+    return objectP
 
+def is_Tree(t):
+    return type(t) in [Tree, ParentedTree, MultiParentedTree]
 
+def findHead(t):
+    if is_Tree(t):
+        ccs = [subtree for subtree in t if is_Tree(subtree) and subtree.label() == "CC"]
+        heads = [subtree for subtree in t if is_Tree(subtree) and (subtree.label().startswith("N") or subtree.label() == "PRP")]
+        if len(ccs) > 0:
+            return heads
+        else:
+            if len(heads) > 0:
+                return heads[-1]
+            else: []
+    else:
+        return []
 
+def findLastNoun(t):
+    if is_Tree(t):
+        if (t.label().startswith("N") and t.label() != "NP") or t.label() == "PRP":
+            if len(t.leaves()[0]) == 1:
+                leftSibling = t.left_sibling()
+                n = leftSibling.leaves()[0] + " " + t.leaves()[0]
+            else:
+                n = t.leaves()[0]
+            return n
+        nps = [subtree for subtree in t if is_Tree(subtree) and (subtree.label().startswith("N") or subtree.label() == "PRP")]
+        if len(nps) > 0:
+            return findLastNoun(nps[-1])
+        else:
+            return []
+    elif t is None:
+        return []
+    else:
+        nps = []
+        for st in t:
+            nps.append(findLastNoun(st))
+        return nps
 
-# for SUBTEXT in re.split("that|whether", EXAMPLE_TEXT):
-#     SUBTEXT = SUBTEXT.strip()
-#     tokens = word_tokenize(SUBTEXT)
-#     pos = pos_tag(tokens)
-#     newPos = morePos(pos)
-#     print (EXAMPLE_TEXT)
-#     print (EXAMPLE_TRIPLET)
-#     chunk = chunk_parser.parse(newPos)
-#     chunk.draw()
-# whether: actual-PREPOSITION expected-CONJUNCTION
-# 명사, 동사 수일치
-# amino: actual-PREPOSITION expected-NOUN
-# NOUN PHRASE에서 마지막 noun만 subject or object로 인정(phrase의 head로 선정)
-# signaling: actual-VERB expected-NOUN
-## 고유명사는 cmudict로 확인할까?
-## Subject: ACTION의 왼쪽에서 가장 가까운 형제 NP의 마지막 NN
-## Object: ACTION이 포함된 VP의 NP의 마지막 NN
-##         ACTION의 오른쪽에서 가장 가까운 형제 NP, PP의 마지막 NN
+def flatten(inputlist):
+    outputList = []
+    for l in inputlist:
+        if type(l) == list:
+            outputList += l
+        else:
+            outputList.append(l)
+    return outputList
+
+def findSVOPhrase(sent, actionList, predictor):
+    chunk = makeConstTree(sent, predictor)
+    constTree = ParentedTree.fromstring(chunk["trees"])
+    verbPhrase = []
+    verbs = []
+    findVerbs(constTree, verbPhrase, verbs, actionList)
+    subjectPhrase = findSubject(verbPhrase, actionList)
+    objectPhrase = findObject(verbPhrase, actionList)
+    tmp = []
+    subjects = []
+    objects = []
+    for sp in subjectPhrase:
+        tmp.append(findHead(sp))
+    for t in tmp:
+        subjects.append(findLastNoun(t))
+    tmp = []
+    for op in objectPhrase:
+        tmp.append(findHead(op))
+    for t in tmp:
+        objects.append(findLastNoun(t))
+    return subjects, verbs, objects
+
+def makeTriples(subjects, verbs, objects):
+    triples = []
+    for i in range(len(verbs)):
+        if i < len(subjects):
+            if type(subjects[i]) == list:
+                subject = " and ".join(subjects[i])
+            else:
+                subject = subjects[i]
+        else:
+            subject = ""
+        if i < len(objects):
+            if type(objects[i]) == list:
+                obList = flatten(objects[i])
+                ob = " and ".join(obList)
+            else:
+                ob = objects[i]
+        else:
+            ob = ""
+        triples.append([subject, verbs[i], ob])
+    return triples
+
+def findSVO(sent, actionList, predictor):
+    subjects, verbs, objects = findSVOPhrase(sent, actionList, predictor)
+    triples = makeTriples(subjects, verbs, objects)
+    return triples
+
+def isSameTriples(right, found):
+    for i, word in enumerate(found):
+        if word != right[i]:
+            return False
+    return True
+
+def countRightTriples(right, found):
+    numRight = 0
+    for i, f in enumerate(found):
+        result = False
+        for j, r in enumerate(right):
+            if isSameTriples(r, f):
+                result = True
+                break
+        if result:
+            numRight += 1
+    return numRight
+
+def evaluation(total, test, right):
+    tp = right
+    fp = test - right
+    fn = total - right
+    precision = tp / test
+    recall = tp / total
+    fScore = 2*tp/(fp + fn + 2*tp)
+    return precision, recall, fScore
+
+def getInput(fileName):
+    inputFile = open (fileName, 'r', newline = '', encoding='utf-8-sig')
+    rdr = csv.reader(inputFile)
+    text = list(rdr)
+    inputFile.close()
+    return text
+
+def init():
+    actionList = ["activate", "activates", "activated",
+            "inhibit", "inhibits", "inhibited",
+            "bind", "binds",
+            "require", "requires", "required",
+            "prevent", "prevents", "prevented"]
+    constPredictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/elmo-constituency-parser-2020.02.10.tar.gz")
+
+    text = getInput('CS372_HW4_input_20170441.csv')
+    outputFile = open('CS372_HW4_output_20170441.csv', 'w', newline = '', encoding='utf-8-sig')
+    wr = csv.writer(outputFile)
+    wr.writerow(["Marked", "Sentence", "Actual Triples", "System Triples", "Cited"])
+    for line in text:
+        cited = line[0]
+        sentence = line[1]
+        triples = line[2].split(", ")
+        marked = line[3]
+        systemTriples = findSVO(sentence, actionList, constPredictor)
+        if marked == "Training":
+            wr.writerow(["Training Sentence", sentence, triples, systemTriples, cited])
+        else:
+            wr.writerow(["Test Sentence", sentence, triples, systemTriples, cited])
+    outputFile.close()
+    
+init()
